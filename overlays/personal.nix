a051b925d90b1d725984f4ade454049901b3c683
Frédéric Menou <frederic.menou@gmail.com>
2021-01-26 18:17:08 +0100

    Apply nix-linter

--- overlays/personal.nix
+++ overlays/personal.nix
@@ -1,8 +1,8 @@
-self: super:
-
-let fetchPackage = definition: path:
-      self.callPackage (self.fetchFromGitHub (builtins.fromJSON (builtins.readFile definition)) + path) {};
-in {
-     posix-toolbox = fetchPackage ./ptitfred-posix-toolbox.json "/nix/default.nix";
-     nix-linter = (fetchPackage ./synthetica9-nix-linter.json "/default.nix").nix-linter;
+let fetchPackage = self: definition: path:
+    self.callPackage (self.fetchFromGitHub (builtins.fromJSON (builtins.readFile definition)) + path) {};
+in
+ self: { ... }:
+   {
+     posix-toolbox = fetchPackage self ./ptitfred-posix-toolbox.json "/nix/default.nix";
+     nix-linter = (fetchPackage self ./synthetica9-nix-linter.json "/default.nix").nix-linter;
    }
