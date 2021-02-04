0e2f2334bdcf7caae445a8d28dcf39d3d4c4dca1
Frédéric Menou <frederic.menou@gmail.com>
2021-02-04 03:24:14 +0100

    PostgreSQL12 with Postgis

--- overlays/personal.nix
+++ overlays/personal.nix
@@ -1,8 +1,11 @@
 let fetchPackage = self: definition: path:
-    self.callPackage (self.fetchFromGitHub (builtins.fromJSON (builtins.readFile definition)) + path) {};
+      self.callPackage (self.fetchFromGitHub (builtins.fromJSON (builtins.readFile definition)) + path) {};
 in
  self: { ... }:
-   {
-     posix-toolbox = fetchPackage self ./ptitfred-posix-toolbox.json "/nix/default.nix";
-     nix-linter = (fetchPackage self ./synthetica9-nix-linter.json "/default.nix").nix-linter;
-   }
+    let postgresql_12_postgis = self.postgresql_12.withPackages (p: [ p.postgis ]);
+    in
+      {
+        posix-toolbox = fetchPackage self ./ptitfred-posix-toolbox.json "/nix/default.nix";
+        inherit (fetchPackage self ./synthetica9-nix-linter.json "/default.nix") nix-linter;
+        inherit postgresql_12_postgis;
+      }
