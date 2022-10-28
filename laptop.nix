c9a052f9a214f6e839a61b952336bb474c35f769
Frédéric Menou <frederic.menou@gmail.com>
2022-10-28 14:15:11 +0200

    Uncommit root home-manager config file
    This file is meant to be private, the rest is configurable and
    versioned.

---
+++ laptop.nix
@@ -0,0 +1,12 @@
+{ ... }:
+
+{
+  imports = [
+    home/nix.nix
+    home/home-manager.nix
+    home/desktop.nix
+    home/development
+    home/network.nix
+    home/others.nix
+  ];
+}
---
+++ tests/home.nix
@@ -0,0 +1,11 @@
+{ ... }:
+
+{
+  imports = [ ../laptop.nix ];
+
+  desktop = {
+    mainColor = "#FF0000";
+    location = { latitude = "44.0003"; longitude = "4.20001"; };
+    spacing = 10;
+  };
+}
