e3588be3103bdfe1734ada6a750b2232db0a670c
Frédéric Menou <frederic.menou@gmail.com>
2023-07-27 16:38:04 +0200

    terminal: Adapt font size

--- home/fonts.nix
+++ home/fonts.nix
@@ -5,5 +5,5 @@
   toPolybar = { name, size }: "${name}:size=${toString size}";
   toI3      = { name, size }: { names = [ name ]; size = size * 1.0; };
   toXFT     = { name, size }: "xft:${name}:pixelsize=${toString size}";
-  toGTK      = { name, size }: "${name} ${toString size}";
+  toGTK     = { name, size }: "${name} ${toString size}";
 }
