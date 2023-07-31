baaf9067fc51bcec827045c8321c4a7638989a34
Frédéric Menou <frederic.menou@gmail.com>
2023-07-31 22:10:21 +0200

    Fix dual options definition

--- home/desktop/wifi.nix
+++ home/desktop/wifi.nix
@@ -3,11 +3,6 @@
 let aeroplane-mode = pkgs.callPackage ./aeroplane-mode {};
  in
 {
-  options.desktop.brightness-step = lib.mkOption {
-    type = lib.types.int;
-    default = 5;
-  };
-
   config = lib.mkIf (! config.desktop.virtual-machine) {
     desktop.i3-extra-bindings = {
       "XF86RFKill" = "exec --no-startup-id ${aeroplane-mode}/bin/aeroplane-mode-toggle";
