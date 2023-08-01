ba4d7926e178447e2f0ce44457104fa1c9e45edc
Frédéric Menou <frederic.menou@gmail.com>
2023-08-01 16:30:42 +0200

    desktop: extract i3

--- home/desktop.nix
+++ home/desktop.nix
@@ -2,21 +2,7 @@
 
 let palette = import ./palette.nix;
 
-    screenshot = pkgs.callPackage desktop/screenshot {};
-    screenshotCmd = "${screenshot}/bin/screenshot";
-
-    inherit (import ./fonts.nix { baseSize = config.desktop.fontSize; }) roboto toI3 toGTK;
-
-    mkWorkspace = index: name: { inherit index name; };
-
-    terminal      = mkWorkspace 1 "Terminal";
-    web           = mkWorkspace 2 "Web";
-    pro           = mkWorkspace 3 "Pro";
-    chat          = mkWorkspace 4 "Chat";
-    files         = mkWorkspace 5 "Files";
-    documentation = mkWorkspace 6 "Documentation";
-    system        = mkWorkspace 8 "Système";
-    capture       = mkWorkspace 9 "Capture";
+    inherit (import ./fonts.nix { baseSize = config.desktop.fontSize; }) roboto toGTK;
 in
   {
     imports = [
@@ -30,6 +16,7 @@ in
       desktop/redshift.nix
       desktop/random-background.nix
       desktop/picom.nix
+      desktop/i3.nix
     ];
 
     options = with lib; {
@@ -75,23 +62,6 @@ in
         '';
         default = "#afd6ff";
         example = "#afd6ff";
-      };
-
-      desktop.spacing = mkOption {
-        type = types.int;
-        description = "";
-        default = 10;
-      };
-
-      desktop.exec-on-login = mkOption {
-        type = types.listOf types.str;
-        default = [];
-        example = [ "sxhkd" ];
-      };
-
-      desktop.i3-extra-bindings = mkOption {
-        type = types.attrsOf types.str;
-        default = {};
       };
     };
 
@@ -149,84 +119,6 @@ in
 
       xdg.configFile."gtk-3.0/colors.css".text = builtins.readFile desktop/colors.css;
 
-      xsession.windowManager.i3 =
-        {
-        enable = true;
-        config =
-          let font = toGTK roboto;
-
-              workspaces =
-                [ terminal
-                  chat
-                  pro
-                  web
-                  files
-                  documentation
-                  system
-                  capture
-                ];
-              foldMap = function: builtins.foldl' (acc: value: acc // function value) {};
-              concat = foldMap (x: x);
-          in {
-            bars = []; # we rely on polybar
-            fonts = toI3 roboto;
-            workspaceAutoBackAndForth = true;
-
-            colors = {
-              focused = lib.mkOptionDefault {
-                border      = lib.mkForce palette.special.background;
-                childBorder = lib.mkForce palette.special.background;
-                indicator   = lib.mkForce config.desktop.mainColor;
-                background = lib.mkForce palette.special.background;
-                text = lib.mkForce config.desktop.mainColor;
-              };
-              unfocused = lib.mkOptionDefault {
-                text = lib.mkForce palette.mate.white;
-              };
-            };
-
-            gaps = {
-              inner = config.desktop.spacing;
-              smartBorders = "on";
-            };
-
-            keybindings =
-              let modifier = config.xsession.windowManager.i3.config.modifier;
-                  bindWorkspace = { index, name } : {
-                    "${modifier}+${builtins.toString index}"       = "workspace number ${builtins.toString index}:${name}";
-                    "${modifier}+Shift+${builtins.toString index}" = "move container to workspace number ${builtins.toString index}:${name}";
-                  };
-                  bindWorkspaces = foldMap bindWorkspace;
-                  mkBindRelease = key: command: {
-                    "--release ${modifier}+${key}" = "exec ${command}";
-                  };
-                  bindings = mkBindRelease "x"     screenshotCmd
-                          // bindWorkspaces workspaces
-                          // config.desktop.i3-extra-bindings;
-               in lib.mkOptionDefault bindings;
-
-            menu = "${pkgs.bemenu}/bin/bemenu-run -l 20 -p '>' -i --fn '${font}' -H 15 --hf '${config.desktop.mainColor}' --tf '${config.desktop.mainColor}'";
-
-            startup =
-              let onStart = command: { inherit command; always = true; notification = false; };
-               in builtins.map onStart config.desktop.exec-on-login;
-
-            assigns =
-              let assignToWorkspace = {index, name} : assignments: { "${builtins.toString index}: ${name}" = assignments; };
-               in concat
-                    [ (assignToWorkspace files         [ { class = "^org.gnome.Nautilus$";       } ])
-                      (assignToWorkspace capture       [ { class = "^.shutter-wrapped$";         } ])
-                      (assignToWorkspace documentation [ { class = "^Zeal$";                     } ])
-                      (assignToWorkspace system        [ { class = "^Com.github.stsdc.monitor$"; } ])
-                    ];
-          };
-        extraConfig =
-          ''
-            for_window [class=".*"] title_format "  %title"
-            for_window [class="(?i)nm-connection-editor"] floating enable, move position center
-            exec i3-msg workspace ${toString terminal.index}:${terminal.name}
-          '';
-      };
 
       xresources.properties = with palette; {
         "*.foreground"  = special.foreground;
