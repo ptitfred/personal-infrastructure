dc5971aabb970a2b8408ff1ea02750f957d37d0d
Frédéric Menou <frederic.menou@gmail.com>
2023-08-12 01:42:47 +0200

    obsidian v1.3.7

--- home/others.nix
+++ home/others.nix
@@ -1,5 +1,10 @@
 { pkgs, lib, ... }:
 
+let obsidian = pkgs.callPackage ./obsidian.nix {
+      version = "1.3.7";
+      sha256 = "sha256-8Qi12d4oZ2R6INYZH/qNUBDexft53uy9Uug7UoArwYw=";
+    };
+in
 {
   home.packages = with pkgs; [
       qemu
@@ -7,10 +12,11 @@
       slack
       ncdu
       gimp
+      obsidian
     ];
 
   nixpkgs.config.allowUnfree = true;
 
   # Necessary for flakes, see https://github.com/nix-community/home-manager/issues/2942#issuecomment-1119760100
-  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [ "slack" ];
+  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [ "slack" "obsidian" ];
 }
