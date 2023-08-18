180c30366681f396bec94eea1f73aa0195c60ab5
Frédéric Menou <frederic.menou@gmail.com>
2023-08-18 21:46:23 +0200

    Rotate login screen's background

--- nixos/workstation.nix
+++ nixos/workstation.nix
@@ -4,7 +4,7 @@ let orange = "#ffb52a";
     user = config.workstation.user;
 
     inherit (inputs.home-manager-base.packages.x86_64-linux) backgrounds;
-    background = "${backgrounds}/pexels-sohail-nachiti-807598.jpg";
+    background = "${backgrounds}/pexels-pok-rie-2049422.jpg";
  in
 {
   options.workstation.user = lib.mkOption { type = lib.types.str; };
