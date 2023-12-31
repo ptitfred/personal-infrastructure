d29fa826af998e480257f9aae9380bbfdec8bfe5
Frédéric Menou <frederic.menou@gmail.com>
2023-12-31 11:37:16 +0100

    Bump nixpkgs & HM to 23.11

--- home/others.nix
+++ home/others.nix
@@ -1,8 +1,8 @@
 { pkgs, lib, ... }:
 
 let obsidian = pkgs.callPackage ./obsidian.nix {
-      version = "1.3.7";
-      sha256 = "sha256-8Qi12d4oZ2R6INYZH/qNUBDexft53uy9Uug7UoArwYw=";
+      version = "1.5.3";
+      sha256 = "sha256-F7nqWOeBGGSmSVNTpcx3lHRejSjNeM2BBqS9tsasTvg=";
     };
 in
 {
@@ -19,4 +19,9 @@ in
 
   # Necessary for flakes, see https://github.com/nix-community/home-manager/issues/2942#issuecomment-1119760100
   nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [ "slack" "obsidian" ];
+
+  # Electron version 25.9.0 is EOL but is used by obsidian.
+  nixpkgs.config.permittedInsecurePackages = [
+    "electron-25.9.0"
+  ];
 }
