b19a01355291fc217b2a8854774d3d99a74bab0e
Frédéric Menou <frederic.menou@gmail.com>
2024-01-19 23:57:40 +0100

    nvim: settings for lua's LSP

--- home/development/neovim-config.lua
+++ home/development/neovim-config.lua
@@ -136,6 +136,14 @@ lsp.marksman.setup {
 }
 lsp.lua_ls.setup {
   on_attach = on_attach,
+  settings = {
+    Lua = {
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = {'vim'},
+      },
+    },
+  },
 }
 
 local hover = require('hover')
