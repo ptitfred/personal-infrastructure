17b69ce11ee245e632e3ad31ab95e85c15ad9e06
Frédéric Menou <frederic.menou@gmail.com>
2024-10-10 17:53:25 +0200

    matomo: upgrade to 5.1.2

--- nixos/personal-infrastructure/matomo.nix
+++ nixos/personal-infrastructure/matomo.nix
@@ -3,6 +3,19 @@
 with lib;
 
 let cfg = config.personal-infrastructure.matomo;
+
+    version = "5.1.2";
+    hash = "sha256-6kR6OOyqwQfV+pRqHO+VMLM1eZQb0om65EilAnIlW9U=";
+
+    # matomo_5 in nixpkgs is at 5.1.1 as of this commit
+    package = pkgs.matomo_5.overrideAttrs {
+      name = "matomo_5-${version}";
+      inherit version;
+      src = pkgs.fetchurl {
+        url = "https://builds.matomo.org/matomo-${version}.tar.gz";
+        inherit hash;
+      };
+    };
 in
 {
   options.personal-infrastructure.matomo = {
@@ -20,6 +33,7 @@ in
   config = mkIf cfg.enable {
     services.matomo = {
       enable = true;
+      inherit package;
       nginx = {};
       inherit (cfg) hostname;
     };
