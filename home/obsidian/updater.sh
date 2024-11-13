7e04441109297467416a252c679b64883caacc8a
Frédéric Menou <frederic.menou@gmail.com>
2024-11-13 12:30:12 +0100

    Obsidian updater: commit changes

--- home/obsidian/updater.sh
+++ home/obsidian/updater.sh
@@ -4,10 +4,16 @@ filename="obsidian-${version}.tar.gz";
 url="https://github.com/obsidianmd/obsidian-releases/releases/download/v${version}/${filename}";
 hash="$(nix-prefetch-url "$url")"
 
-jq . > "home/obsidian/version.json" <<- JSON
+target_file=home/obsidian/version.json
+
+jq . > "$target_file" <<- JSON
   {
     "version": "$version",
     "sha256": "$hash",
     "url": "$url"
   }
 JSON
+
+git add "$target_file"
+
+git commit -m "Obsidian v$version" -- "$target_file"
