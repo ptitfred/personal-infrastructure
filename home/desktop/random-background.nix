8edd73c5470cc1d625cb38c5fec7b108284500a8
Frédéric Menou <frederic.menou@gmail.com>
2024-12-22 17:31:53 +0100

    Extract backgrounds as top-level package

--- home/desktop/random-background.nix
+++ home/desktop/random-background.nix
@@ -1,11 +1,9 @@
 { pkgs, ... }:
 
-let backgrounds = pkgs.callPackage ./backgrounds {};
-in
 {
   services.random-background = {
     enable = true;
-    imageDirectory = backgrounds.outPath;
+    imageDirectory = pkgs.backgrounds.outPath;
     interval = "20 minutes";
   };
 }
--- home/tools.nix
+++ home/tools.nix
@@ -2,7 +2,6 @@
 
 {
   rofi-screenshot    = callPackage desktop/rofi-screenshot     {};
-  backgrounds        = callPackage desktop/backgrounds         {};
   toggle-redshift    = callPackage desktop/toggle-redshift.nix {};
   focus-by-classname = callPackage desktop/focus-by-classname  {};
   aeroplane-mode     = callPackage desktop/aeroplane-mode      {};
