bc9603787da5970470f0e3240f8d0bd6992bc3e9
Frédéric Menou <frederic.menou@gmail.com>
2025-01-09 12:10:17 +0100

    Patch obsidian to support Wayland

--- pkgs/obsidian/package.nix
+++ pkgs/obsidian/package.nix
@@ -3,4 +3,5 @@
 obsidian.overrideAttrs {
   src = fetchurl { inherit url sha256; };
   inherit version;
+  commandLineArgs = "--no-sandbox --ozone-platform=wayland --ozone-platform-hint=auto --enable-features=UseOzonePlatform,WaylandWindowDecorations %U";
 }
